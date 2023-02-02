50ec4d885cf60df4d891e3c7b647f74f6d0cc16b
teefa <gad993813@gmail.com>
2023-02-02 14:54:17 +0200

    new version

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-let input = document.querySelector("input");
+let inputKey = document.querySelector(".input-key");
+let inputValue = document.querySelector(".input-value");
 let buttons = document.querySelectorAll(".buttons span");
 let result = document.querySelector(".result");
-
 buttons.forEach((span) => {
     span.addEventListener("click", (e) => {
 
@@ -21,12 +21,12 @@ buttons.forEach((span) => {
 //check item
 function checkItem() {
     console.log("check");
-    if (input.value !== "") {
+    if (inputKey.value !== "") {
         //check local storage
-        if (localStorage.getItem(input.value)) {
-            result.innerHTML = `Found Local Item Called <span>${input.value}</span>`;
+        if (localStorage.getItem(inputKey.value)) {
+            result.innerHTML = `Found Local Item Called <span>${inputKey.value}</span>`;
         } else {
-            result.innerHTML = `No Local Storage Item With The Name <span>${input.value}</span>`;
+            result.innerHTML = `No Local Storage Item With The Name <span>${inputKey.value}</span>`;
         }
     } else {
         showMassage();
@@ -37,11 +37,12 @@ function checkItem() {
 }
 //add item
 function addItem() {
-    if (input.value !== "") {
+    if (inputKey.value !== "" && inputValue.value !== "") {
         //check local storage
-        localStorage.setItem(input.value, "text");
-        result.innerHTML = ` Local Item  <span>${input.value} </span>Added`;
-        input.value = "";
+        localStorage.setItem(inputKey.value, inputValue.value);
+        result.innerHTML = ` Local Item  <span>${inputKey.value} </span>Added`;
+        inputKey.value = "";
+        inputValue.value = "";
 
     } else {
         showMassage();
@@ -51,14 +52,15 @@ function addItem() {
 }
 //delete item
 function deleteItem() {
-    if (input.value !== "") {
+    if (inputKey.value !== "") {
         //check local storage
-        if (localStorage.getItem(input.value)) {
-            localStorage.removeItem(input.value);
-            result.innerHTML = ` Local Item <span>${input.value} </span>Deleted`;
-            input.value = "";
+        if (localStorage.getItem(inputKey.value)) {
+            localStorage.removeItem(inputKey.value);
+            result.innerHTML = ` Local Item <span>${inputKey.value} </span>Deleted`;
+            inputKey.value = "";
+            inputValue.value = "";
         } else {
-            result.innerHTML = `No Local Storage Item With The Name <span>${input.value}</span>`;
+            result.innerHTML = `No Local Storage Item With The Name <span>${inputKey.value}</span>`;
         }
     } else {
         showMassage();
@@ -66,16 +68,54 @@ function deleteItem() {
 
 
 }
+
 //show item
 function showItem() {
+
     if (localStorage.length) {
         result.innerHTML = "";
-
+        //create table
+        let table = document.createElement("table");
+        //create head and row
+        let head = document.createElement("thead");
+        let headRow = document.createElement("tr");
+        let keyHead = document.createElement("td");
+        let valueHead = document.createElement("td");
+        //create text 
+        let keyHeadText = document.createTextNode("key");
+        let valueHeadText = document.createTextNode("value");
+        //append text to td
+        keyHead.appendChild(keyHeadText);
+        valueHead.appendChild(valueHeadText);
+        //apppend row of head to head
+        head.appendChild(headRow);
+        //append td to row
+        headRow.appendChild(keyHead);
+        headRow.appendChild(valueHead);
+        //append head to table
+        table.appendChild(head);
         for (let [key, value] of Object.entries(localStorage)) {
+            //create row    
+            let row = document.createElement("tr");
+            //create td
+            let tdKey = document.createElement("td");
+            let tdValue = document.createElement("td");
+            //create text to td
+            let keyText = document.createTextNode(key);
+            let ValueText = document.createTextNode(value)
+            //append text in td
+            tdKey.appendChild(keyText);
+            tdValue.appendChild(ValueText)
+            //append td to row
+            row.appendChild(tdKey);
+            row.appendChild(tdValue);
+            //append row to table
+            table.appendChild(row);
 
-            result.innerHTML += `<span class="value">${key}</span>`;
+
         }
-
+        //append table to result
+        result.appendChild(table);
     } else {
         result.innerHTML = "Local Storge Is Empty ";
     }
@@ -85,11 +125,9 @@ function showItem() {
 
 
 //return massage if input is empty
+
 function showMassage() {
-
     result.innerHTML = "Input is Empty";
-
-
 }
 
 
